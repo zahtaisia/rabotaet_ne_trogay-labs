b0d31079d997800bbdf6afb07b971e8c31d50a29
Захарова Таисия <tostorik@MacBook-Air--Taisiia.local>
2026-09-11 13:43:16 +0300

    Add images

--- lab_01/src/02_sum_avg.py
+++ lab_01/src/02_sum_avg.py
@@ -2,4 +2,4 @@ a = float(input('Введите первое число: ').replace(',', '.'))
 b = float(input('Введите втрое число:'))
 sum = a + b
 avg = round(sum / 2, 2)
-print(f'{sum=}, {avg=}')
+print(f'{sum=}; {avg=}')
